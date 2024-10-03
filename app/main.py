c60cc74390a29b5ed01cb576f27b02aba5247fc0
seramm <sergioaramburu@tutanota.com>
2024-10-03 13:45:06 +0200

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+from pathlib import Path
+
+app = FastAPI()
+
+
+@app.get("/get_img/{img}")
+async def get_image(img: str):
+    image_path = Path("../data/" + img)
+    if not image_path.is_file():
+        return {"error": "Image not found on the server"}
+    return FileResponse(image_path)
