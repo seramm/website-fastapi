66983c5d043b2a3477235408c24639b8e7774826
seramm <sergioaramburu@tutanota.com>
2024-10-13 13:10:27 +0200

    test

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import health
 from pathlib import Path
 
 app = FastAPI()
 
+app.include_router(health.router)
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        )
 
 @app.get("/get_img/{img}")
 async def get_image(img: str):
@@ -12,3 +22,9 @@ async def get_image(img: str):
     if not image_path.is_file():
         return {"error": "Image not found on the server"}
     return FileResponse(image_path)
+
+@app.get("/get_n_img/")
+async def get_n_imgs():
+    path = Path("/srv/data/")
+    n = len([f for f in path.iterdir() if f.is_file()])
+    return {"number": n}
---
+++ app/routers/health.py
@@ -0,0 +1,24 @@
+from typing import Literal
+from typing_extensions import TypedDict
+from fastapi import APIRouter, status
+
+STATUS = Literal["success", "error", "partial", "unknown"]
+
+class ReturnHealtcheckStruct(TypedDict):
+    status: STATUS
+
+router = APIRouter(
+        prefix="/v1/health-check",
+        tags=["Health Check"],
+        )
+
+@router.get(
+        "/liveness",
+        summary="Perform a health check",
+        response_description="Return HTTP Status Code 200 (OK)",
+        status_code=status.HTTP_200_OK,
+        response_model=ReturnHealtcheckStruct,
+        )
+
+async def liveness() -> ReturnHealtcheckStruct:
+    return {"status" : "success"}
